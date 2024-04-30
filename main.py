1f315808c9dd8ddab6051f120b8625d195111d65
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-29 19:56:14 -0700

    email notification setup

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 import requests
 import selectorlib
+
+from send_email import send_email
 
 URL = 'http://programmer100.pythonanywhere.com/tours/'
 
 # Making sure scrape attempt isn't getting blocked.
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+password = os.getenv("GMAIL_PASSWORD")
+sender = os.getenv("SENDER")
+receiver = os.getenv("RECEIVER")
 
 
 def scrape(url):
@@ -19,10 +26,6 @@ def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
-
-
-def send_email(value):
-    print("Sending email")
 
 
 def store(extracted):
@@ -42,4 +45,4 @@ if __name__ == '__main__':
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email(extracted)
+            send_email(extracted, sender, password, receiver)
